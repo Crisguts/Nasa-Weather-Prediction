3d3697619fdeb2989d410be744046328368f2994
Nicholas Roy <nicholas@patroy.ca>
2025-10-04 10:58:19 -0400

    Added fetch functionality

--- nasa-weather-prediction/app.js
+++ nasa-weather-prediction/app.js
@@ -2,10 +2,16 @@
 // app.js - minimal JS to toggle between single date and range inputs
 
 document.addEventListener('DOMContentLoaded', function () {
+    // Elements
     const modeSelect = document.getElementById('modeSelect');
     const singleGroup = document.getElementById('singleDateGroup');
     const rangeGroup = document.getElementById('rangeDateGroup');
     const predictBtn = document.getElementById('predictBtn');
+    // Vars
+    const startDate = "20000101";
+    const endDate = getCurrentDateYYYYMMDD();
+    var latitude = 0.0;
+    var longitude = 0.0;
 
     function updateMode() {
         const mode = modeSelect.value;
@@ -23,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-    predictBtn.addEventListener('click', function (e) {
+    predictBtn.addEventListener('click', async function (e) {
         e.preventDefault();
         const mode = modeSelect.value;
         if (mode === 'range') {
@@ -36,5 +42,26 @@ document.addEventListener('DOMContentLoaded', function () {
             console.log('Predict single', { date });
             alert(`Predict for date:\n${date || '(empty)'}`);
         }
+        const data = await fetchData(startDate, endDate, latitude, longitude);
+        console.log(data);
     });
 });
+
+
+
+async function fetchData(startDate, endDate, latitude, longitude) {
+  const request = new Request(`https://power.larc.nasa.gov/api/temporal/daily/point?start=${startDate}&end=${endDate}&latitude=${latitude}&longitude=${longitude}&parameters=T2M_MAX&community=RE&format=JSON`);
+  const response = await fetch(request);
+  const jsonData = await response.json();
+  // console.log(jsonData);
+  return jsonData;
+
+}
+
+function getCurrentDateYYYYMMDD() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0'); // Month is 0-indexed
+  const day = String(now.getDate()).padStart(2, '0');
+  return `${year}${month}${day}`;
+}
